9408b6c11e7957c3ae43ef38ed8979bfebc8d4ec
华成志 <1397713787@qq.com>
2018-11-01 14:41:26 +0800

    添加出库接口

--- src/main/java/com/wantaihao/farming_demo1/repository/AnimalDeliverRepository.java
+++ src/main/java/com/wantaihao/farming_demo1/repository/AnimalDeliverRepository.java
@@ -14,6 +14,6 @@ import java.util.Date;
 public interface AnimalDeliverRepository extends JpaRepository<AnimalDeliver ,Integer> {
     @Modifying
     @Transactional
-    @Query(nativeQuery = true,value = "INSERT INTO animal_deliver (animal_deliver.animal_type,\tanimal_deliver.unit_price,animal_deliver.animal_num,animal_deliver.animal_log_id,animal_deliver.trans_date,animal_deliver.buyer_info)VALUES (?1,?2,?3,?4,?5,?6)")
+    @Query(nativeQuery = true,value = "INSERT INTO animal_deliver (animal_deliver.animal_type,tanimal_deliver.unit_price,animal_deliver.animal_num,animal_deliver.animal_log_id,animal_deliver.trans_date,animal_deliver.buyer_info)VALUES (?1,?2,?3,?4,?5,?6)")
     int addDeliver(String animalType, BigDecimal unitPrice, BigDecimal animalNum, int animalLogId, Date transDate, String buyerInfo);
 }
