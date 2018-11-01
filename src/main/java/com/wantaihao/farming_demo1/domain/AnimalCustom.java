1880ee1be33416986b68b6f562efb1f66e2c6ac7
华成志 <1397713787@qq.com>
2018-11-01 14:42:36 +0800

    添加出库接口

--- src/main/java/com/wantaihao/farming_demo1/domain/AnimalCustom.java
+++ src/main/java/com/wantaihao/farming_demo1/domain/AnimalCustom.java
@@ -1,11 +1,13 @@
 package com.wantaihao.farming_demo1.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class AnimalCustom {
     private Integer CustomId;
     private int CustomType;//客户类型
