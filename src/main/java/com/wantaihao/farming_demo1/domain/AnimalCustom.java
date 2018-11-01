719b2dc66d29f497e58a3b3db5741e36af0cf9bc
华成志 <1397713787@qq.com>
2018-11-01 15:23:28 +0800

    添加出库接口

--- src/main/java/com/wantaihao/farming_demo1/domain/AnimalCustom.java
+++ src/main/java/com/wantaihao/farming_demo1/domain/AnimalCustom.java
@@ -16,5 +16,14 @@ public class AnimalCustom {
     private int CustomType;//客户类型
     @Column(nullable = false)
     private String CustomName;//客户名称
+    @Column(nullable = false)
+    private String CustomPhone;//电话号码
+    @Column(nullable = false)
+    private String CustomAddr;//地址
+    @Column(nullable = false)
+    private String CustomMail;//电子邮箱
+    @Column(nullable = false ,columnDefinition = "default 1")
+    private int CustomStatus;
+
 
 }
